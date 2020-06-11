33a993c44f19be70ef733947f3b3d829cb479d95
17721250181 <60857118+17721250181@users.noreply.github.com>
2020-06-12 09:39:34 +1200

    a

---
+++ student1/search_form/result.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Greeting</title>
+  </head>
+  <body>
+    <?php
+    // get the firstname
+    $firstname_serch = trim($_POST['name']);
+    echo "$firstname_serch";
+    //connect to database
+    $db_connect = mysqli_connect("localhost","root","","studentdb");
+    // create Query
+    $student_sql = "SELECT student.studentID, student.firstname, student.lastname, tutorgroup.name FROM student JOIN tutorgroup ON student.tutorgroupID = tutorgroup.tutorgroupID WHERE student.firstname=$firstname_serch";
+    // run Query
+    $student_qry = mysqli_query($db_connect,$student_sql);
+    // store result to array
+    $student_aa = mysqli_fetch_assoc($student_qry);
+
+    //output the name
+    echo $student_aa['studentID'].' '.$student_aa['firstname'].' '.$student_aa['lastname'].'  '.$student_aa['name'];
+    ?>
+
+     <form action="serch.php" method="post">
+       <button type="submit" name="button">Back</button>
+     </form>
+  </body>
+</html>
---
+++ student1/search_form/serch.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Welcome</title>
+  </head>
+  <body>
+    <form action="result.php" method="post">
+      <h1>Put the firstname you want to serch</h1>
+      <input type="text" name="name" placeholder="Firstname">
+      <button type="submit" name="button">Confirm</button>
+    </form>
+  </body>
+</html>
