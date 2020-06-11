bb345b4bdf2d11147e845cbd24a8cecc48f90d39
17721250181 <60857118+17721250181@users.noreply.github.com>
2020-06-12 09:51:26 +1200

    serch

--- student1/search_form/result.php
+++ student1/search_form/result.php
@@ -2,6 +2,7 @@
 <html lang="en" dir="ltr">
   <head>
     <meta charset="utf-8">
+    <link rel="stylesheet" href="styles.css">
     <title>Greeting</title>
   </head>
   <body>
@@ -10,18 +11,17 @@
     if(!isset($_POST['name']))header('Location: serch.php');
     // get the firstname
     $firstname_serch = trim($_POST['name']);
-    echo "$firstname_serch";
     //connect to database
     $db_connect = mysqli_connect("localhost","root","","studentdb");
     // create Query
-    $student_sql = "SELECT student.studentID, student.firstname, student.lastname, tutorgroup.name FROM student JOIN tutorgroup ON student.tutorgroupID = tutorgroup.tutorgroupID WHERE student.firstname='$firstname_serch'";
+    $student_sql = "SELECT student.studentID, student.firstname, student.lastname, tutorgroup.name FROM student JOIN tutorgroup ON student.tutorgroupID = tutorgroup.tutorgroupID WHERE student.firstname LIKE '%$firstname_serch%'";
     // run Query
     $student_qry = mysqli_query($db_connect,$student_sql);
     // store result to array
     $student_aa = mysqli_fetch_assoc($student_qry);
 
     //output the name
-    echo $student_aa['studentID'].' '.$student_aa['firstname'].' '.$student_aa['lastname'].'  '.$student_aa['name'];
+    echo 'StudentID: '.$student_aa['studentID'].'<p></p>Name(Firstname-Lastname): '.$student_aa['firstname'].' '.$student_aa['lastname'].'<p></p>Turotgroup:'.$student_aa['name'];
     ?>
 
      <form action="serch.php" method="post">
